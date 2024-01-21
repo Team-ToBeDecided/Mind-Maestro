54378939dcc2368763c24d2c7216d7afeeaff6c9
Dhruv1238 <dhruv4075@gmail.com>
2024-01-21 12:27:35 +0530

    fix: Crash reason Identified

--- client/src/components/PomodoroTimer.jsx
+++ client/src/components/PomodoroTimer.jsx
@@ -9,7 +9,7 @@ const PomodoroTimer = () => {
 
     useEffect(() => {
         let timerId;
-        if (isRunning && !timeLeft) return 
+        if (isRunning && !timeLeft) return
 
         if (isRunning) {
             const timerId = setTimeout(() => {
@@ -21,7 +21,7 @@ const PomodoroTimer = () => {
     }, [timeLeft, isRunning]);
 
     useEffect(() => {
-        if(timeLeft === 0) {
+        if (timeLeft === 0) {
             alert('Time for Break!');
             setIsBreak(!isBreak);
             setTimeLeft(isBreak ? 5 * 60 : 25 * 60) //switch for work and break
@@ -34,16 +34,18 @@ const PomodoroTimer = () => {
         return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     };
     return (
-        <Card style={{ position: 'fixed', top: '20px', left: '20px', zIndex: 1000, backgroundColor: 'black' }}>
+        <Card className=' bg- shadow-lg drop-shadow-[50px_50px_50px_rgba(0,0,0,0.05)] backdrop-blur-sm absolute left-16 top-10'>
             <CardBody>
-            <div className='flex'>
-                <span className='text-5xl font-action'>Time Left: {formatTime()}</span>
-                    <Button onClick={() => setIsRunning(!isRunning)}>
-                        {isRunning ? <PauseIcon className="h-2 w-2" /> : <PlayIcon className="h-2 w-2" />}
-                    </Button>
-                    <Button onClick={() => setTimeLeft(0)}>
-                        <StopIcon className="h-2 w-2" />
-                    </Button>
+                <div className='flex flex-col gap-3'>
+                    <span className='text-5xl font-action text-black'>Time Left- {formatTime()}</span>
+                    <div className='flex gap-5 items-center justify-center'>
+                        <Button onClick={() => setIsRunning(!isRunning)} color='indigo'>
+                            {isRunning ? <PauseIcon className="h-3 w-3" /> : <PlayIcon className="h-3 w-3" />}
+                        </Button>
+                        <Button onClick={() => setTimeLeft(0)} color='indigo' >
+                            <StopIcon className="h-3 w-3" />
+                        </Button>
+                    </div>
                 </div>
             </CardBody>
         </Card>
--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import Timer from '../components/Timer';
 import MusicPlayer from '../components/Music';
-import { Popover, PopoverHandler, PopoverContent } from '@material-tailwind/react';
+import { Popover, PopoverHandler, PopoverContent, IconButton } from '@material-tailwind/react';
 import image1 from '../assets/himalaya.jpg';
 import image2 from '../assets/simson.jpg';
 import image3 from '../assets/snow.jpeg'
 import PomodoroTimer from '../components/PomodoroTimer';
+import { GlobeAltIcon } from '@heroicons/react/24/solid';
 const images = [image1, image2, image3];
 
 const Room = () => {
-  const [walli, setWalli] = useState(image1); // Use image3 as initial state
+  const [walli, setWalli] = useState(image1);
+  const [task, setTask] = useState('');
 
   const changeBackgroundImage = (newImagePath) => {
     setWalli(newImagePath);
@@ -26,26 +28,27 @@ const Room = () => {
   }, [walli]);
 
   return (
-    <> <div style={{ backgroundImage: `url(${walli})` }}>
-      <Popover placement="bottom-start" >
-        <PopoverHandler>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 absolute right-5 top-10 ml-2">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-          </svg>
-        </PopoverHandler>
-        <PopoverContent className="flex bg-transparent backdrop-blur-sm">
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Image ${index + 1}`}
-              style={{ width: '4vw', height: '4vw', margin: '0.5vw', cursor: 'pointer' }}
-              onClick={() => changeBackgroundImage(image)}
-            />
-          ))}
-        </PopoverContent>
-      </Popover>
-    </div>
+    <>
+      <div style={{ backgroundImage: `url(${walli})` }}>
+        <Popover placement="bottom-start" >
+          <PopoverHandler>
+            <IconButton className="text-black w-6 h-6 fixed right-5 top-10">
+              <GlobeAltIcon className="h-6 w-6" />
+            </IconButton>
+          </PopoverHandler>
+          <PopoverContent className="flex bg-transparent backdrop-blur-sm">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Image ${index + 1}`}
+                style={{ width: '4vw', height: '4vw', margin: '0.5vw', cursor: 'pointer' }}
+                onClick={() => changeBackgroundImage(image)}
+              />
+            ))}
+          </PopoverContent>
+        </Popover>
+      </div>
       <div className="flex justify-between w-full mt-4 items-baseline">
         {/* <Timer /> */}
         <PomodoroTimer />
